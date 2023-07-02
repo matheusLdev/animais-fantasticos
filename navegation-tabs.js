b7d2d25a3534ac4db3e835ae3d882ab0027dac23
Matheus Gonçalves <goncalves.mld@hotmail.com>
2023-07-02 15:09:37 -0300

    add navegation-tabs.js

---
+++ navegation-tabs.js
@@ -0,0 +1,20 @@
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+document.documentElement.className += " js";
+
+if(tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add('active');
+
+  function activeTab(index) {
+    tabContent.forEach((section) => {
+      section.classList.remove('active');
+    });
+    tabContent[index].classList.add('active');
+  }
+
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+      activeTab(index);
+    });
+  });
+}
