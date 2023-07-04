3b110cd672f3c468f0da7a3004b743bc30f0fac5
Matheus Gonçalves <goncalves.mld@hotmail.com>
2023-07-04 17:03:27 -0300

    add scroll smooth

---
+++ js/scroll-smooth.js
@@ -0,0 +1,18 @@
+function scrollSmooth() {
+  const internalLinks = document.querySelectorAll("a[href^='#']");
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+  });
+  }
+
+  internalLinks.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+scrollSmooth();
