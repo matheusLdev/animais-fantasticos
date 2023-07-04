748105e0d89f0ca615628f300ae2c2363487d560
Matheus Gonçalves <goncalves.mld@hotmail.com>
2023-07-04 17:36:48 -0300

    add scroll animate

---
+++ js/scroll-animate.js
@@ -0,0 +1,20 @@
+function scrollAnimate() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const halfWindow = window.innerHeight / 2;
+
+    function livenScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const sectionVisible = (sectionTop - halfWindow) < 0;
+        if (sectionVisible) {
+          section.classList.add("active");
+        };
+      });
+    };
+    livenScroll();
+    
+    window.addEventListener("scroll", livenScroll);
+  };
+};
+scrollAnimate();
