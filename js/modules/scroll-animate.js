ed7357bc4e9d43962dfbb9a8bd37fe3595da5915
Matheus Gonçalves <goncalves.mld@hotmail.com>
2023-07-06 20:36:17 -0300

    Scroll animate transformada em class

--- js/modules/scroll-animate.js
+++ js/modules/scroll-animate.js
@@ -1,20 +1,41 @@
-export default function scrollAnimate() {
-  const sections = document.querySelectorAll("[data-anime='scroll']");
-  function livenScroll() {
-    const halfWindow = window.innerHeight / 2;
-    sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const sectionVisible = (sectionTop - halfWindow) < 0;
-      if (sectionVisible) {
-        section.classList.add("active");
-      } else if (section.classList.contains("active")) {
-        section.classList.remove("active");
+export default class ScrollAnimate {
+  constructor(section) {
+    this.sections = document.querySelectorAll(section);
+    this.halfWindow = window.innerHeight / 2;
+
+    this.checkDistance = this.checkDistance.bind(this);
+  }
+
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.halfWindow),
+      };
+    });
+  }
+
+  checkDistance() {
+    this.distance.forEach(item => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add("active");
+      } else if (item.element.classList.contains("active")) {
+        item.element.classList.remove("active");
       };
     });
   }
   
-  if (sections.length) { 
-    livenScroll();
-    window.addEventListener("scroll", livenScroll);
-  };
+  init() {
+    if (this.sections.length) {
+      this.getDistance()
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
+    };
+    return this;
+  }
+
+  stop() {
+    window.removeEventListener("scroll", this.checkDistance);
+  }
 }
